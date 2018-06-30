abcd1071b4257efbc50afb54b761e0699cb6c0a1
Ycooi <33864420+Ooiyenchun@users.noreply.github.com>
2018-06-30 13:07:52 +0800

    Second Commit
    C++ programming

--- Practical/P6Q5/P6Q5/P6Q5.c
+++ Practical/P6Q5/P6Q5/P6Q5.c
@@ -10,18 +10,45 @@ Date         : 29-6-2018
 
 int CreditHour();
 int Calculation();
-int Display();
 
 int main()
 {
-	TotalHour();
 	Calculation();
-	Display();
-
 	system("pause");
 	return 0;
 }
 int CreditHour()
 {
-	printf("Please enter the number of credit hour you completed : ")
+	int hour;
+	printf("Please enter the number of credit hour you completed : ");
+	scanf("%d", &hour);
+	return hour;
+}
+
+int Calculation()
+{
+	int freshmanLowest = 0, sophomoreLowest = 45, juniorLowest = 90, seniorlowest = 135,totalHour;
+	totalHour = CreditHour();
+
+	if (totalHour >=seniorlowest)
+	{
+		printf("\nYour ranking is Senior.\nHappy Studying!\n");
+	}
+	else if(totalHour>=juniorLowest)
+	{
+		printf("\nYour ranking is Junior.\nHappy Studying!\n");
+	}
+	else if (totalHour >= sophomoreLowest)
+	{
+		printf("\nYour ranking is Sophomore.\nHappy Studying!\n");
+	}
+	else if (totalHour >=freshmanLowest)
+	{
+		printf("\nYour ranking is Freshman.\nHappy Studying!\n");
+	}
+	else
+	{
+		printf("Wrong input,please try again!");
+	}
+	return 0;
 }
